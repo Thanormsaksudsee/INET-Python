ca8dfca3c98e13df38aa5aa1cf8108fe87ae0e48
BIG <s6506022420037@email.kmutnb.ac.th>
2022-07-22 22:37:16 +0700

    MyPrint1

---
+++ MyBasic age21/Print72122.py
@@ -0,0 +1,33 @@
+print('Hello world')
+print('Information and Network engineering')
+print('Hello Thanormsak you will be great')
+print("I'm humgry")
+print('He asl me "Who got the book!"')
+print("I'm good and \"I will do my best\"")
+print('/\\') 
+print('\ta\na\ta\ta\n\ta')
+print('''\ta\na\ta\ta\n\ta''')
+print('100.00')
+print('%d' %100)
+print('%d' %100.52)
+print('%f' %-100.89)
+print('%.2f' %100.89)
+from math import pi
+print('%.2f' %pi)
+print('%.4f' %pi)
+print('%.50f' %pi)
+print('My age is' , 25, 'I have' , 3500.50,'Bth.')
+print('My age is %d I gave %.2f Bth.' %(25,3500.50))
+print('My age is '+ str(25)+' I have '+ str(3500.50)+' Bth.')
+print('5+4=' ,5+4)
+print('5+4 = %.2f' %(6+4))
+print('5+4 = ' +str(5+4))
+print("I'm anirach", "I'll keep practicing!")
+print(1,2,3,4,5,6,7,8,)
+print('%d %d %d %d %d' %(1,2,3,4,5))
+
+
+
+
+
+
