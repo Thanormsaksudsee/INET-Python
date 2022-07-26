b1a4098bc738c0b0f3c91b387d9cbf3c329f5aac
BIG <s6506022420037@email.kmutnb.ac.th>
2022-07-26 21:30:44 +0700

    Datatype

---
+++ MyBasic age21/2Datatype72622.py
@@ -0,0 +1,9 @@
+# ชื่อตัวแปร = ค่าที่เก็บในตัวแปร
+x = 20 #integer     
+y = 3.99
+z = True
+print("ผลลัพธ์ "+str(x))
+print(y)
+print(z)
+print(type(y))
+
---
+++ MyBasic age21/3Type Conversion72622.py
@@ -0,0 +1,15 @@
+x = 10
+y = 2.99
+z = "2" #"2" = > 2
+
+# บวกเลข
+
+result = x+y # 10+2.99 = 12.99
+result = x+int(z)#เปลี่ยน เลขสตริงเป็นข้อมูเลขด้วยคำสั่ง int(ตัวแปร)
+result = x+float(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(result)
+
