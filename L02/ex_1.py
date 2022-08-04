bd4de9de54108ceb1cd29072329a13653e1e226a
thanormsaksudsee <s6506022420037@email.kmutnb.ac.th>
2022-08-04 10:37:55 +0700

    8422

---
+++ L02/ex_1.py
@@ -0,0 +1,6 @@
+sales = int(input('sales value : ')) 
+if sales > 50000:
+    bonus = 500.0
+    commision_rat = 0.12
+    print('You met your sales quota!')
+    
---
+++ L02/ex_2.py
@@ -0,0 +1,5 @@
+termperrature = float(input('Temperrature Value : '))
+if termperrature < 40:
+    print("A little cold, isn't it?")
+else:
+    print("Nice weather we're having.")
