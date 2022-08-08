e4419cd01cc5d247213e70669ee72ca9ea43e931
BIG <s6506022420037@email.kmutnb.ac.th>
2022-08-09 00:39:26 +0700

    8922

---
+++ MyBasic age21/for.py
@@ -0,0 +1,26 @@
+'''print('Number\tSquare')
+print('______________')
+
+for number in range(1, 50):
+    square = number**2
+    print(number, '\t', square)'''
+    
+'''for x in range(5):
+    print('Hello world')
+print('Range start end')
+for num in range(1, 5):
+    print(num)
+print('Range start, end, step')
+for num in range(1, 10, 2):
+    print(num)'''
+    
+'''print('KPH\t  MPH')
+
+
+for KPH in range(60, 140, 10):
+    MPH = KPH*0.6214
+    print(KPH, '\t', '%.1f' %MPH)'''
+    
+    
+
+    
