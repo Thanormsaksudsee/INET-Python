2d2f93001ed981a80b6aa15168fc6c6c189f0700
BIG <s6506022420037@email.kmutnb.ac.th>
2022-08-18 01:15:28 +0700

    81822

--- L03test/1-100.py
+++ L03test/1-100.py
@@ -3,8 +3,5 @@ colum = int(input('Enter Colum'))
 
 for i in range(1,101):
     print(i,end=' ')
-    for j in range(1,100,colum):
-        if i == j:
-            
-            print('\n')
-        
+    if i % colum == 0:
+        print()
---
+++ L03test/1-100/2.py
@@ -0,0 +1,8 @@
+Colum = int(input("Enter column"))
+num = 0
+while num <= 100 :
+    print(num,end=' ')
+    num = num+1
+    if num % Colum == 0:
+        print('\n')
+
