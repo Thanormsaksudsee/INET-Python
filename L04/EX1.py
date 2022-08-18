930a913ffee5fa5f8fa3df712c20ccf08164cfd1
thanormsaksudsee <s6506022420037@email.kmutnb.ac.th>
2022-08-18 09:44:33 +0700

    81822

---
+++ L04/EX1.py
@@ -0,0 +1,7 @@
+First = str(input('Enter your first name: '))
+Last = str(input('Enter your lasst name: '))
+ID = (input('Enter your student ID number: '))
+total = First[:3]+Last[:3]+ID[-3:]
+print('You system login name is: ',total)
+
+
