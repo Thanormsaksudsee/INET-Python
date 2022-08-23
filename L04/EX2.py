3b042acad8fa90ce4dd258743cb3ca8ec1c353b7
thanormsaksudsee <s6506022420037@email.kmutnb.ac.th>
2022-08-23 13:10:09 +0700

    11082322

--- L04/EX2.py
+++ L04/EX2.py
@@ -1,16 +1,19 @@
-
 name = 'anirachmingkhan'
-index = 0
 
-while index < len(name) :
-    print(name.upper()[index],end='')
-    index += 1
-    if index < len(name) :
-        print(name.lower()[index],end='')
-        index += 1
+Start = 1
+uper = 3
 
 
-#ใหญ่1แล็ก1  ใหญ่2เล็ก1 ใหญ๋3เล็ก1
+for i in range(len(name)):
+    if i == Start :
+        print(name[i],end='')
+        Start += uper
+        uper += 1
+    else:
+        print(name[i].upper(),end='')
+
+        
+        #ใหญ่1แล็ก1  ใหญ่2เล็ก1 ใหญ๋3เล็ก1
 
 
 
