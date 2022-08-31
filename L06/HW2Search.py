877bf2fb8d9dc000111851e3a91a81f7272464b0
BIG <s6506022420037@email.kmutnb.ac.th>
2022-09-01 03:17:12 +0700

    123456789

--- L06/HW2Search.py
+++ L06/HW2Search.py
@@ -1,9 +1,19 @@
 def main():
-    fr = open("Display.txt", "r")
-    fi = fr.readline()
+    emp_file = open("Display.txt",'r')
+    name = input('Enter name your want to find : ')
+    for mem in emp_file:
+        see = mem.find(name)
+        keep = emp_file.readline()
+        print(see)
+        if see > -1 :
+            print('Name:',mem.strip())
+            print('ID:',keep.strip())
+            keep = emp_file.readline()
+            print('dept:',keep.strip())
+            keep = emp_file.readline()
+            
+            
 
-    name = input("Specific name record: ")
 
-    while 
-
+    emp_file.close()
 main()
---
+++ L06/test2.py
@@ -0,0 +1,18 @@
+import re
+fr = open("Display.txt",'r')
+name = input('Enter name your want to find : ')
+
+for mem in fr:
+    see = re.search(name,mem)
+    keep = fr.readline()
+    print(see)
+    if  see != None :
+        print('Name:',mem.strip())
+        print('ID:',keep.strip())
+        keep = fr.readline()
+        print('dept:',keep.strip())
+        keep = fr.readline()
+
+
+
+fr.close()
