1e6c3abaf12e11fd53c186494292f095d0d946d1
BIG <s6506022420037@email.kmutnb.ac.th>
2022-09-21 16:50:10 +0700

    Homework

---
+++ L07/HomeWorkR.py
@@ -0,0 +1,65 @@
+heroes = ['Ironman', 'Thor', 'Hulk', 'Superman', 'Spiderman']
+
+def Display():
+    print(heroes)
+    
+def add():
+    Display()
+    Hero = input('Add Heroes : ')
+    heroes.append(Hero)
+    Display()
+def Insert():
+    Display()
+    Hero = input('Add Heroes : ')
+    index = int(input('Insert Heroes :'))
+    heroes.insert(index,Hero)
+    Display()
+    
+    
+    
+def Remove():
+    Display()
+    Hero = input('Remove Heroes : ')
+    heroes.remove(Hero)
+    Display()
+    
+def DisSort():
+    heroes.sort()
+    Display()
+def Exits():
+    name = input('Exits Heroes : ')     
+    if(name in heroes ):
+        print(name , 'found')
+    if(name not in heroes):
+        print(name , 'not found')    
+
+def main():
+    keep = 'y'
+    while keep == 'y':
+        print('1.Display Heroes Funtion')
+        print('2.Add Heroes Funtion')
+        print('3.Insert Heroes Funtion')
+        print('4.Remove Heroes Funtion')
+        print('5.Display Sorted Heroes Funtion')
+        print('6.Exist Funtion')
+        Select = int(input('Please Seclect Funtion : '))
+        
+        if Select == 1:
+            Display()
+        elif Select == 2:
+            add()
+        elif Select == 3:
+            Insert()
+        elif Select == 4:
+            Remove()
+        elif Select == 5:
+            DisSort()
+        elif Select == 6:
+            Exits()
+            
+        keep = input('Enter y for Moretime: ')
+    
+
+main()
+
+
--- L07/Homwork.py
+++ L07/Homwork.py
@@ -1,9 +1,40 @@
-def Display():
+def main()
     
-    heroes = ['Ironman', 'Thor', 'Hulk', 'Superman', 'Spiderman']
+    def Display():
+        heroes = ['Ironman', 'Thor', 'Hulk', 'Superman', 'Spiderman']
+        print(print)
+    Display()
 
-    print(heroes)
-Display()
+    def add():
+    
+        heroes = ['Ironman', 'Thor', 'Hulk', 'Superman', 'Spiderman']
+        heroes.append('BIGKUMA')
+        print(heroes)
+
+    add()
 
 
 
+    def Remove():
+        heroes = ['Ironman', 'Thor', 'Hulk', 'Superman', 'Spiderman']
+        heroes.remove('Ironman')
+        print(heroes)
+    Remove()
+
+    def DisSort():
+        heroes = ['Ironman', 'Thor', 'Hulk', 'Superman', 'Spiderman']
+        heroes.sort()
+        print(heroes)
+    DisSort()
+
+    def Exis():
+        name = input('Hero name')     
+        heroes = ['Ironman', 'Thor', 'Hulk', 'Superman', 'Spiderman']
+        if(name in heroes ):
+            print(name , 'found')
+        if(name not in heroes):
+            print(name , 'not found')
+    Exis()
+        
+        
+main()
