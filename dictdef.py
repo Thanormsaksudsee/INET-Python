e68cc91d1556f8a88da062bb793b887dd750e9f5
Your Name <Thanormsudsee>
2022-10-25 15:45:59 +0700

    update final

---
+++ dictdef.py
@@ -0,0 +1,8 @@
+duck = {'name': "Jonh", 'age': 21, 'budget': 23000},
+{'name': "Steve", 'age': 32, 'budget': 40000},
+{'name': "Martin", 'age': 16, 'budget': 2700}
+
+for i in duck:
+    print(i)
+    for j in i.keys:
+        print(j)
---
+++ testFinal.py
@@ -0,0 +1,147 @@
+'''from math import floor
+def numberSplit(num):
+    num1 = floor(num/2)
+    num2 = num - num1
+
+
+    return [num1,num2]
+print(numberSplit(4))
+print(numberSplit(10))
+print(numberSplit(11))
+print(numberSplit(-9))'''
+
+
+
+'''def highestDigit(dig): 
+    i = str(dig)  
+    return max(i)
+print(highestDigit(379))
+print(highestDigit(2))
+print(highestDigit(377401))'''
+
+
+# def capTofront(Duck):
+#     list1 = []
+#     list2 = []
+#     for i in Dug:
+#         if i == i.upper():
+#             list1.append(i)
+#         elif i == i.lower():
+#             list2.append(i)
+#     return ''.join(list1 + list2)
+    
+# print(capTofront("hApPy"))
+# print(capTofront("moveMENT"))
+# print(capTofront("shOrtCAKE"))
+
+
+
+'''import math
+
+def totalVolume(Ped):
+    dict = []
+    result = 1
+    for i in Ped :
+        dict.append(i)
+
+
+    return math.prod(dict)
+
+print(totalVolume([4,2,4],[3,3,3]))'''
+
+
+
+
+
+# def getBudgets(IBT):
+#     list1 = []
+#     for key in IBT:
+#         for value in IBT.key['name']:
+#             return key[value]
+
+
+# print(getBudgets([
+# {'name': "Jonh", 'age': 21, 'budget': 23000},
+# {'name': "Steve", 'age': 32, 'budget': 40000},
+# {'name': "Martin", 'age': 16, 'budget': 2700}]))
+
+
+
+
+# def removeDups(Butter):
+#     list1 = []
+    
+#     for i in Butter:
+#         if i != Butter:
+#             pass
+#             list1.append(i)
+#     return list1
+
+
+# print(removeDups(["The", 'big', 'The']))
+
+
+
+
+
+
+# def sumTwoSmallestNums(Duck):
+#     list1 = []
+#     list2 = []
+
+#     for i in Duck:
+#         if min(Duck):
+#             list1.append(i)
+#     for i in Duck:
+#         if min(Duck):
+#             list1.append(i)
+#     result = list1 + list2
+
+#     return result
+
+# print(sumTwoSmallestNums([19,5,42,2,77]))
+
+
+
+
+# Duck = [19,5,42,2,77]
+# list1 = []
+# list2 = []
+
+# for i in Duck:
+#     if i == min(Duck):
+#         list1.append(i) 
+# del Duck[min(Duck)]
+# for i in Duck:
+#     if min(Duck):
+#         list2.append(i)
+#     result = list1 + list2
+# print (result)
+
+
+
+
+
+
+
+
+# sim = ([1,2,3,],3)
+# list1 = []
+# for i in sim:
+#     for j in i:
+#         if j 
+#         print(j)
+
+
+
+
+
+# Pedclock = 5025
+# PedclockH = Pedclock // 3600
+# PedclockH1 = Pedclock % 3600
+# PedclockM = PedclockH1 // 60
+# PedclockS = PedclockH1 % 60
+
+
+
+# print(PedclockH,PedclockM,PedclockS)
